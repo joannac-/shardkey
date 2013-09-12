2458cb4470feb563ce1f05300d4dcf3c8de808e4
joanna.cheng@10gen.com <joannac@Joannas-MacBook-Pro.local>
2013-09-12 16:34:21 +1000

    Added document count

--- bars.py
+++ bars.py
@@ -26,6 +26,9 @@ class ShardVis(object):
         self.infile = open(str(infile), "r")
         self.number_shards = len(data)
         self.chunk_size = 64
+        self.doccount = []
+        for i in xrange(self.number_shards):
+            self.doccount.append(0)
 
         self.data = data
 
@@ -46,7 +49,8 @@ class ShardVis(object):
             pos = np.arange(0, -len(shard_data), -1) - .5
             self.artists = plt.barh(pos, [ch.size for ch in shard_data], align='center')
 
-            ax.set_ylim(-max_chunks, 0)
+            x = max(max_chunks, 5)
+            ax.set_ylim(-1*x, 0)
             ax.set_xlim(0, 10)
             ax.axes.get_xaxis().set_visible(False)
             ax.axes.get_yaxis().set_visible(False)
@@ -54,10 +58,17 @@ class ShardVis(object):
             #self.artists[0].set_color((0.4, 0.2, 0.4))
 
 
+
+        for shard in xrange(self.number_shards):
+            ax = plt.subplot(1, self.number_shards, shard+1)
+            xcoord = shard/self.number_shards + 0.25
+            plt.text(xcoord, 0.025, '#docs = ' + str(self.doccount[shard]))
+
         # next button
-        axes = plt.axes([0.8, 0.025, 0.1, 0.04])
+        axes = plt.axes([0.9, 0.025, 0.08, 0.04])
         self.button_next = plt.Button(axes, 'Next')
         self.button_next.on_clicked(self._click_next)
+
 
         # randomize button
 #        axes = plt.axes([0.025, 0.025, 0.1, 0.04])
@@ -68,29 +79,32 @@ class ShardVis(object):
 
 
     def _click_next(self, event):
-        line = self.infile.readline()
-        if not line:
-            print "No more lines!"
-            return
-        toks = line.split()
-        sh = int(toks[1])
-        cl = int(toks[2])
-        if toks[0] == "add":
-            self.data[sh][cl].incr();
-            print "add"
-        elif toks[0] == "split":
-            currsize = self.data[sh][cl].getsize()
-            nsize = int(toks[3])
-            self.data[sh][cl].setsize(nsize)
-            self.data[sh].append(Chunk(currsize-nsize))
-            print "split"
-        elif toks[0] == "move":
-            news = int(toks[3])
-            chunk = self.data[sh].pop(cl)
-            self.data[news].append(chunk)
-            print "move"
-            
-        self._render()
+        for i in xrange(5):
+            line = self.infile.readline()
+            if not line:
+                print "No more lines!"
+                return
+            toks = line.split()
+            sh = int(toks[1])
+            cl = int(toks[2])
+            if toks[0] == "add":
+                self.data[sh][cl].incr();
+                self.doccount[sh] += 1
+                print "add"
+            elif toks[0] == "split":
+                currsize = self.data[sh][cl].getsize()
+                nsize = int(toks[3])
+                self.data[sh][cl].setsize(nsize)
+                self.data[sh].append(Chunk(currsize-nsize))
+                print "split"
+            elif toks[0] == "move":
+                news = int(toks[3])
+                chunk = self.data[sh].pop(cl)
+                self.data[news].append(chunk)
+                self.doccount[sh] -= chunk.getsize()
+                self.doccount[news] += chunk.getsize()
+                print "move"
+            self._render()
         
 #        self.number_bars += 1
 #        self.data = random.random(self.number_bars)
